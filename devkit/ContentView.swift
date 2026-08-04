1a184ebf5e9a218bce01e3cc9f788ce37f24e62a
zhihua.zhang <zhihua.zhang@kunlun-inc.com>
2026-08-04 12:03:22 +0800

    Initial Commit

---
+++ devkit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  devkit
+//
+//  Created by kl on 2026/8/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ devkit/devkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  devkitApp.swift
+//  devkit
+//
+//  Created by kl on 2026/8/4.
+//
+
+import SwiftUI
+
+@main
+struct devkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
